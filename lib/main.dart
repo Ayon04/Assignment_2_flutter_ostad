8f9e434b045276b6a9a9a8712965902da3d230ef
Project <gayon125@gmail.com>
2024-11-12 18:37:04 +0600

    assignment

--- lib/main.dart
+++ lib/main.dart
@@ -8,61 +8,101 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: MyProfileScreen(),
       debugShowCheckedModeBanner: false,
-      title: 'My Application',
-      home: Scaffold(
-        backgroundColor: Colors.white70,
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurpleAccent,
-          titleTextStyle: TextStyle(fontSize: 40),
-          title: Text('Home'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.lightGreenAccent,
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  textStyle: TextStyle(fontSize: 20),
-                ),
-                onPressed: () {
-                  print('Thank you');
-                },
-                child: Text('Click'),
-              ),
-              SizedBox(height: 20), // Adds some spacing between buttons
-              TextButton(
-                onPressed: () {
-                  print('Text button clicked');
-                },
-                child: Text('Tap here',style: TextStyle(
-
-                  fontSize: 30,
-                ),),
-              ),
-
-              IconButton(onPressed: (){
-
-                print('Iconnn button clicked');
-
-              }, icon: Icon(Icons.add)),
-
-              OutlinedButton(onPressed: (){
+    );
 
 
-                print('Outline button clicked');
+  }
+}
 
-
-              },
-
-
-                  child:Text('click'))
-            ],
+class MyProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Profile'),
+        backgroundColor: Colors.yellow,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
           ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.phone),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ProfileItem(
+              icon: Icons.icecream,
+              label: 'Ice cream is very delicious right?',
+            ),
+            ProfileItem(
+              icon: Icons.code,
+              label: 'Programming is not boring if you love it',
+            ),
+            ProfileItem(
+              icon: Icons.egg,
+              label: 'If you submit code directly copy from chatgpt then mark will 0',
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+
+class ProfileItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const ProfileItem({
+    required this.icon,
+
+
+    required this.label,
+  });
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 95,
+            backgroundColor: Colors.purple[90],
+            child: Icon(
+              icon,
+              size: 40,
+              color: Colors.purple[10],
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
